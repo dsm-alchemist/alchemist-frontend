b9237f841ca67e802fab9eaf4042245899f4c7c7
seockhun <hun00719@gmail.com>
2022-01-25 15:51:07 +0900

    [CREATE] init file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Base} from "./assets/base.jpeg";
+import MainRouter from './route';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (  
+    <>
+      <MainRouter></MainRouter>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ route/mainRouter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+const MainRouter = () => {
+    return(
+        <Router>
+            <Switch>
+
+            </Switch>
+        </Router>
+    )
+}
+
+export default MainRouter;
