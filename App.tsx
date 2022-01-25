ed704f1f6ce6a3738faf47c8ef47020d73ac349c
seockhun <hun00719@gmail.com>
2022-01-25 16:15:51 +0900

    [CREATE] header

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import {Base} from "./assets/base.jpeg";
 import MainRouter from './route';
 
 export default function App() {
-  return (  
+  return (
     <>
       <MainRouter></MainRouter>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  
-});
--- route/mainRouter.tsx
+++ route/mainRouter.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Header from "../components/public/header/header";
 
 const MainRouter = () => {
     return(
         <Router>
             <Switch>
-
+                <Header />
             </Switch>
         </Router>
     )
